a85104bd7c3ba45181d3d2f351c62195c4ce09f5
yingmengfeiy <290423696@qq.com>
2019-06-05 22:13:49 +0800

    first commit

---
+++ src/test/java/com/shijie/base/TestDemo.java
@@ -0,0 +1,12 @@
+package com.shijie.base;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @Test
+    public void testcase1(){
+        Assert.assertTrue(false);
+        System.out.println("testcase1");
+    }
+}
